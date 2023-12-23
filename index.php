9c291f5f68adcf0fbd1b5a9d590f26ea165a865d
Mirsandev <xmirandadevx@gmail.com>
2023-12-23 09:41:11 -0400

    PDO

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 
 <?php
-require 'includes/funciones.php';
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+require 'includes/app.php';
 incluirTemplate('header', $inicio = true);
 ?>
 
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'mery/bienesraices_inicio',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '13fe36e358448f9be4e5e0f06716f6b5d0500c1a',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'mery/bienesraices_inicio' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '13fe36e358448f9be4e5e0f06716f6b5d0500c1a',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
